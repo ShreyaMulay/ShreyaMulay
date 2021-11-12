1bf272b22fb51793561f621df2f96a7e242d6d15
Shreya <73992105+shreyamulay@users.noreply.github.com>
2021-11-12 16:31:56 +0530

    Add files via upload

---
+++ library/views.py
@@ -0,0 +1,159 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from . import forms,models
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import Group
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required,user_passes_test
+from datetime import datetime,timedelta,date
+from django.core.mail import send_mail
+from librarymanagement.settings import EMAIL_HOST_USER
+from django.shortcuts import redirect
+
+
+def home_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'library/index.html')
+
+
+def studentclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'library/studentclick.html')
+
+#for showing signup/login button for teacher
+def adminclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'library/adminclick.html')
+
+
+
+def adminsignup_view(request):
+    form=forms.AdminSigupForm()
+    if request.method=='POST':
+        form=forms.AdminSigupForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            user.set_password(user.password)
+            user.save()
+
+
+            my_admin_group = Group.objects.get_or_create(name='ADMIN')
+            my_admin_group[0].user_set.add(user)
+
+            return HttpResponseRedirect('adminlogin')
+    return render(request,'library/adminsignup.html',{'form':form})
+
+
+
+
+
+
+def studentsignup_view(request):
+    form1=forms.StudentUserForm()
+    form2=forms.StudentExtraForm()
+    mydict={'form1':form1,'form2':form2}
+    if request.method=='POST':
+        form1=forms.StudentUserForm(request.POST)
+        form2=forms.StudentExtraForm(request.POST)
+        if form1.is_valid() and form2.is_valid():
+            user=form1.save()
+            user.set_password(user.password)
+            user.save()
+            f2=form2.save(commit=False)
+            f2.user=user
+            user2=f2.save()
+
+            my_student_group = Group.objects.get_or_create(name='STUDENT')
+            my_student_group[0].user_set.add(user)
+
+        return HttpResponseRedirect('studentlogin')
+    return render(request,'library/studentsignup.html',context=mydict)
+
+
+
+
+def is_admin(user):
+    return user.groups.filter(name='ADMIN').exists()
+
+def afterlogin_view(request):
+    if is_admin(request.user):
+        return render(request,'library/adminafterlogin.html')
+    else:
+        return render(request,'library/studentafterlogin.html')
+
+
+@login_required(login_url='adminlogin')
+@user_passes_test(is_admin)
+def addbook_view(request):
+    #now it is empty book form for sending to html
+    form=forms.BookForm()
+    if request.method=='POST':
+        #now this form have data from html
+        form=forms.BookForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            return render(request,'library/bookadded.html')
+    return render(request,'library/addbook.html',{'form':form})
+
+@login_required(login_url='adminlogin')
+@user_passes_test(is_admin)
+def viewbook_view(request):
+    books=models.Book.objects.all()
+    return render(request,'library/viewbook.html',{'books':books})
+
+
+
+
+
+
+
+@login_required(login_url='adminlogin')
+@user_passes_test(is_admin)
+def viewstudent_view(request):
+    students=models.StudentExtra.objects.all()
+    return render(request,'library/viewstudent.html',{'students':students})
+
+
+
+def vbook(request):
+    books=models.Book.objects.all()
+    return render(request,'library/vbook.html',{'books':books})
+
+
+
+def destroy(request, id):  
+    book =models.Book.objects.get(id=id)  
+    book.delete()  
+    return redirect("/")
+
+def edit(request):
+    id=request.GET['id']
+    name=isbn=author="Not_Available"
+    for data in models.Book.objects.filter(id=id):
+        name=data.name
+        isbn=data.isbn
+        author=data.author
+        
+    return render(request,'library/edit.html',{'id':id,'name':name,'isbn':isbn,'author':author})
+
+
+
+def RecordEdited(request):
+    if request.method == 'POST':
+        id=request.POST['id']
+        name=request.POST['name']
+        isbn=request.POST['isbn']
+        author=request.POST['author']
+        models.Book.objects.filter(id=id).update(name=name,isbn=isbn,author=author)
+        return HttpResponseRedirect('viewbook')
+    else:
+        return HttpResponse("404 - Not Found")
+
+        
+        
+  
+
+    
